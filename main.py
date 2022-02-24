7590097ab3603b1977fcce42d9f15b60cd9c86b7
Samson <91712993+samtoch3@users.noreply.github.com>
2022-02-24 02:59:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 24 01:44:17 2022
+Created on Tue Feb 15 15:57:37 2022
 
 @author: Samson N.
-
--- Controler un navigateur avec python
--- doc : https://selenium-python.readthedocs.io/index.html
--- doc : https://pypi.org/project/selenium/
-
-module nécessaire :
-    pip install selenium
-    
 """
+import random
+import PySimpleGUI as sg
 
 
-from selenium import webdriver
+def tirage(personnes):
+    isChoisi = {}
+    ListNumero = []
+    numeroAttribue = {}
+
+    for i in personnes.keys():
+        isChoisi[i]=False
+        ListNumero.append(i)
+        
+    s = 0
+    
+    for i in ListNumero:
+        while  True:
+            while True:
+                s = random.choice(ListNumero)
+                if isChoisi[s]==False:
+                    break
+            if s!=i:
+                break
+        
+        numeroAttribue[i] = s
+        isChoisi[s] = True
+
+    return numeroAttribue    
+    
+    
+def affichage(personnes, numeroAttribue):
+    for i in numAttribue.keys():
+    sg.popup('Resultat - Tirage au sort', f'\n{personnes[i]}, ----->>> {personnes[numAttribue[i]]}.')
 
 
-# Creer un variable pour declencher le driver
-driver = webdriver.Chrome(executable_path='chromedriver.exe')
-driver.get('https://www.kubii.fr/')
+nb = ''
+while not(nb.isnumeric()):
+    nb = sg.popup_get_text(f'Entrer le nombre de personnes', 'Tirage au sort')
 
-# recuperer dans la barre de recherche
-search_bar = driver.find_element_by_id('search_query_top')
-search_bar.send_keys('Arduino')
+dico_pers = {}
+#dico_pers = personnes
+for i in range(1,int(nb)+1):
+    n = sg.popup_get_text(f'Entrer le nom et le numero de la personne {i} séparé d\'un espace', f'Personne {i}')
+    x = n.split()
+    dico_pers[int(x[1])] = x[0]
 
-# recuperer le bouton pour lancer la recherche
-search_btn = driver.find_element_by_id('searchbox')
-search_btn.click()
-
-# recuperer tous les noms des produits
-all_products = driver.find_elements_by_class_name('product-name')
-
-# parcourir la liste des produits
-for title in all_products:
-    print(title.text)
-
-
-driver.quit()
-
+numAtt = {}
+numAtt = tirage(dico_pers)
+affichage(dico_pers, numAtt)
